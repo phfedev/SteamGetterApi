62d577cffefad9d46bc36cacc2805d3c64791441
Pedro Henrique Freitas de Paula <phfedev@gmail.com>
2020-03-09 14:52:50 -0300

    Added a route to get a list of games for the SteamID

--- index.js
+++ index.js
@@ -15,12 +15,15 @@ server.get('/steaminfo/:steamId', async(req, res) => {
   res.send(steamInfo)
 })
 
+server.get('/steamgames/:steamId', async(req, res) => {
+  const steamGames = await getSteamGames(req.params.steamId)
+  res.send(steamGames)
+})
+
 async function getSteamInfo(steamId){
   const url = `http://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key=${steamKey}&steamids=${steamId}`
   const fullReq = await axios.get(url).then((resp) => {
     const fullResponse = resp.data.response.players[0]
-    const playerNick = fullResponse.personaname
-    const playerId = JSON.stringify(fullResponse.steamid)
     return JSON.stringify(fullResponse)
   }).catch((error => console.log(error)))
   return fullReq
@@ -30,10 +33,56 @@ async function getSteamId(steamNick){
   const url = `http://api.steampowered.com/ISteamUser/ResolveVanityURL/v0001/?key=${steamKey}&vanityurl=${steamNick}`
   const steamId = await axios.get(url).then((resp) => {
     return JSON.stringify(resp.data.response.steamid)
-    //console.log(JSON.stringify(resp.data.response.steamid))
   })
   .catch((error) => console.log(error))
   return steamId
 }
 
+async function getSteamGames(steamId){
+  const url = `http://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/?key=${steamKey}&steamid=${steamId}&format=json`
+  
+  const steamGames = await axios.get(url).then((resp) => {
+    return resp.data.response
+  }).catch((error) => console.log(error));
+
+  const games = steamGames.games
+  const gameIds = []
+  const gameNames = []
+  for(game in games){
+    gameIds.push(games[game].appid)
+    
+  }
+  for(pos in gameIds){
+    let gameName = await getGameInfo(gameIds[pos])
+    if(gameName != ""){
+      gameNames.push(gameName)
+    }
+  }
+
+  return gameNames
+}
+
+async function getGameInfo(gameId){
+  
+  const url = `https://store.steampowered.com/api/appdetails?appids=${gameId}`
+  let gameName ="";
+  const gameInfo = await axios.get(url).then((resp) => {
+    return resp.data
+  }).catch((error) => console.log(error))
+
+  Object.keys(gameInfo).forEach(function (key) {
+    if(gameInfo[key].success == true){
+      console.log(key)
+      console.log(gameInfo[key].data.name);
+      gameName = gameInfo[key].data.name
+    }else{
+      return
+    }
+  });
+
+  return gameName
+}
+
 server.listen(3000)
+
+
