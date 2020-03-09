d75706e1837c235c5e7bbd9701b14f2b72171261
Pedro Henrique Freitas de Paula <phfedev@gmail.com>
2020-03-09 12:16:35 -0300

    Route to get steam id and rou to get perfil info added

--- index.js
+++ index.js
@@ -1,26 +1,33 @@
 const express = require('express')
 const axios = require('axios')
-
+require('dotenv').config()
 const server = express()
 server.use(express.json())
 const steamKey = process.env.STEAM_API_KEY
-server.get('/steam/:steamNick', async(req, res) => {
-  //const urlGetSteamIdByNick = `http://api.steampowered.com/ISteamUser/ResolveVanityURL/v0001/?key=DA58E9E5030428339131C3A32583A210&vanityurl=${req.params.steamNick}`
+server.get('/steamid/:steamNick', async(req, res) => {
   const steamId = await getSteamId(req.params.steamNick)
-  const url = `http://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key=DA58E9E5030428339131C3A32583A210&steamids=${steamId}`
-  axios.get(url).then((resp) => {
-    //response.players[0].steamid
-    const player = resp.data
-    console.log(JSON.stringify(player.response.players[0].personaname))
-  })
-  res.send('test')
-  //res.send('SteamID' + req.params.id)
+  
+  res.send(steamId)
 })
 
+server.get('/steaminfo/:steamId', async(req, res) => {
+  const steamInfo = await getSteamInfo(req.params.steamId)
+  res.send(steamInfo)
+})
 
+async function getSteamInfo(steamId){
+  const url = `http://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key=${steamKey}&steamids=${steamId}`
+  const fullReq = await axios.get(url).then((resp) => {
+    const fullResponse = resp.data.response.players[0]
+    const playerNick = fullResponse.personaname
+    const playerId = JSON.stringify(fullResponse.steamid)
+    return JSON.stringify(fullResponse)
+  }).catch((error => console.log(error)))
+  return fullReq
+}
 
 async function getSteamId(steamNick){
-  const url = `http://api.steampowered.com/ISteamUser/ResolveVanityURL/v0001/?key=DA58E9E5030428339131C3A32583A210&vanityurl=${steamNick}`
+  const url = `http://api.steampowered.com/ISteamUser/ResolveVanityURL/v0001/?key=${steamKey}&vanityurl=${steamNick}`
   const steamId = await axios.get(url).then((resp) => {
     return JSON.stringify(resp.data.response.steamid)
     //console.log(JSON.stringify(resp.data.response.steamid))
