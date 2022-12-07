b7548c81b753e0cf7b19581cbb66be06b74ed2c0
Pedro Henrique Freitas de Paula <pedro.freitas@numih.com>
2022-12-07 10:55:11 -0300

    update versions

--- controller/steamController.js
+++ controller/steamController.js
@@ -2,29 +2,23 @@ const axios = require('axios')
 require('dotenv').config()
 
 const steamKey = process.env.STEAM_API_KEY
-
 module.exports = {
-  
-  async getSteamFriends(steamId){
+  async getSteamFriends(steamId) {
     const friendsId = []
     const friendsNick = []
-    //friendslist.friends[0].steamid
     const url = `http://api.steampowered.com/ISteamUser/GetFriendList/v0001/?key=${steamKey}&steamid=${steamId}&relationship=friend`
-    const fullReq = await axios.get(url).then(async(resp) => {
-      for(pos in resp.data.friendslist.friends){
+    const fullReq = await axios.get(url).then(async (resp) => {
+      for (pos in resp.data.friendslist.friends) {
         friendsId.push(resp.data.friendslist.friends[pos].steamid)
         let friend = await this.getSteamInfo(friendsId[pos])
         friendsNick.push(friend.personaname)
-        //response.players[0].personaname
-        console.log(friendsNick)
       }
-      //const fullResponse = resp.data.response.players[0]
       return friendsNick
     }).catch((error => console.log(error)))
     return fullReq
   },
-  
-  async getSteamInfo(steamId){
+
+  async getSteamInfo(steamId) {
     const url = `http://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key=${steamKey}&steamids=${steamId}`
     const fullReq = await axios.get(url).then((resp) => {
       const fullResponse = resp.data.response.players[0]
@@ -32,57 +26,55 @@ module.exports = {
     }).catch((error => console.log(error)))
     return fullReq
   },
-  
-  async getSteamId(steamNick){
+
+  async getSteamId(steamNick) {
     const url = `http://api.steampowered.com/ISteamUser/ResolveVanityURL/v0001/?key=${steamKey}&vanityurl=${steamNick}`
     const steamId = await axios.get(url).then((resp) => {
       return JSON.stringify(resp.data.response.steamid)
     })
-    .catch((error) => console.log(error))
+      .catch((error) => console.log(error))
     return steamId
   },
-  
-  async getSteamGames(steamId){
+
+  async getSteamGames(steamId) {
     const url = `http://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/?key=${steamKey}&steamid=${steamId}&format=json`
-    
+
     const steamGames = await axios.get(url).then((resp) => {
       return resp.data.response
     }).catch((error) => console.log(error));
-  
+
     const games = steamGames.games
     const gameIds = []
     const gameNames = []
-    for(game in games){
+    for (game in games) {
       gameIds.push(games[game].appid)
     }
-    for(pos in gameIds){
+    for (pos in gameIds) {
       let gameName = await this.getGameInfo(gameIds[pos])
-      if(gameName != ""){
+      if (gameName != "") {
         gameNames.push(gameName)
       }
     }
-  
     return gameNames
   },
-  
-  async getGameInfo(gameId){
-    
+
+  async getGameInfo(gameId) {
+
     const url = `https://store.steampowered.com/api/appdetails?appids=${gameId}`
-    let gameName ="";
+    let gameName = "";
     const gameInfo = await axios.get(url).then((resp) => {
       return resp.data
     }).catch((error) => console.log(error))
-  
+
     Object.keys(gameInfo).forEach(function (key) {
-      if(gameInfo[key].success == true){
+      if (gameInfo[key].success == true) {
         console.log(key)
         console.log(gameInfo[key].data.name);
         gameName = gameInfo[key].data.name
-      }else{
+      } else {
         return
       }
     });
-  
     return gameName
   }
 }
--- index.js
+++ index.js
@@ -1,18 +1,10 @@
 const express = require('express')
-
-var cors = require('cors')
+let cors = require('cors')
 const server = express()
 const routes = require('./routes')
-
-
 server.use(express.json())
 server.use(cors())
-
-
 server.use(routes)
-
-
-
 server.listen(3000)
 
 
